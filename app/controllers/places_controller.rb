695f603c4629984dc785e73a797392ae25b3a379
Micah Rensch <micah.rensch@kellogg.northwestern.edu>
2024-02-20 14:37:53 +0000

    update

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,4 +7,16 @@ class PlacesController < ApplicationController
   def show
     @place = Place.find_by({"id" => params["id"]})
   end 
+
+  def new
+  
+  end
+
+  def create
+    @place = Place.new
+    @place["name"] = params["name"]
+    @place.save
+    redirect_to "/places"
+  end
+
 end
